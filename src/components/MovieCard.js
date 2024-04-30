1527d70f2fd593ebf503e5f1a8af9a5fb7a0a082
Anjul <anjulsharma010@gmail.com>
2024-04-30 21:21:28 +0530

    final push

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -7,8 +7,10 @@ const MovieCard =({movie}) =>{
     return(
         <div className="card">
                 <img src={!moviemainphotos ? "https://t4.ftcdn.net/jpg/02/51/95/53/360_F_251955356_FAQH0U1y1TZw3ZcdPGybwUkH90a3VAhb.jpg" : moviemainphotos} alt="No Image" />
-                <h4>{movietitle}</h4>
-                <h5>MovieGenres: {moviegenres.join(", ")}</h5>
+                <h2>{movietitle}</h2>
+                <h5><span>MovieGenres: </span>{moviegenres.join(", ")}</h5>
+                <h5><span>MovieLanguages: </span>{movielanguages.join(", ")}</h5>
+                <h5><span>MovieCountries: </span>{moviecountries.join(", ")}</h5>
             </div>
     )
 }
